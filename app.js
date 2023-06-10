5747045d4e0302577cf2e4738762d8320ffaef1f
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2023-06-11 02:20:20 +0530

    Minor Changes related to drop

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const gameBoard = document.querySelector('#gameboard')
 const playerDisplay = document.querySelector('#player')
 const infoDisplay = document.querySelector('#info-display')
 const width = 8
+let playerGo= 'black'
+playerDisplay.textContent = 'black'
+
+
 
 const startPieces = [
     rook, knight, bishop ,queen, king, bishop, knight, rook,
@@ -44,3 +48,63 @@ function createBoard() {
 }
 
 createBoard()
+
+
+const allSqaures = document.querySelectorAll(".square")
+
+allSqaures.forEach( square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+    console.log(e.target)
+    const taken = e.target.classList.contains('piece')
+
+
+    // e.target.parentNode.append(draggedElement)
+    // e.target.remove()
+    // e.target.append(draggedElement)
+    changePlayer()
+
+}
+
+function changePlayer() {
+    if(playerGo === 'black'){
+        reverseIds()
+        playerGo = 'white'
+        playerDisplay.textContent = 'white'
+    }else{
+        revertIds()
+        playerGo = 'black'
+        playerDisplay.textContent = 'black'
+    }
+}
+
+
+function reverseIds(){
+    const allSqaures = document.querySelectorAll(".square")
+    allSqaures.forEach( (square, i ) =>
+        square.setAttribute('square-id', (width*width -1) -i ))
+}
+
+function revertIds() {
+    const allSqaures = document.querySelectorAll(".square")
+    allSqaures.forEach( (square, i ) =>{
+        square.setAttribute('square-id',i )
+    })
+}
