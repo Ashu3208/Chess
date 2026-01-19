b279caf7f71f704212d0df4831dfa8983764cb5c
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2026-01-19 10:35:21 +0530

    added remaining external css to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-      rollupOptions: {
-        external: ["@lichess-org/chessground/assets/chessground.base.css"]
-      }
-    }
-})
+    rollupOptions: {
+      external: [
+        "@lichess-org/chessground/assets/chessground.base.css",
+        "@lichess-org/chessground/assets/chessground.brown.css",
+        "@lichess-org/chessground/assets/chessground.cburnett.css",
+      ],
+    },
+  },
+});
