eb581161de91725f06fc4902cd6304e162e6faeb
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2026-01-19 10:41:03 +0530

    removed external options

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: [
-        "@lichess-org/chessground/assets/chessground.base.css",
-        "@lichess-org/chessground/assets/chessground.brown.css",
-        "@lichess-org/chessground/assets/chessground.cburnett.css",
-      ],
-    },
-  },
 });
